338fb4cb9d8fc1693aa98e2ea235ab3c9610bb94
arup-paul <aruppaulctg@gmail.com>
2022-10-17 00:15:12 +0600

    product seeder and migration

---
+++ database/migrations/2022_10_16_175738_create_products_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('section_id')->constrained()->onDelete('cascade');
+            $table->foreignId('brand_id')->constrained()->onDelete('cascade');
+            $table->foreignId('vendor_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('admin_type');
+            $table->string('product_name');
+            $table->string('product_color');
+            $table->string('product_code');
+            $table->string('product_price');
+            $table->string('product_discount');
+            $table->string('product_weight');
+            $table->string('product_image');
+            $table->string('description');
+            $table->string('meta_title');
+            $table->string('meta_keywords');
+            $table->string('meta_description');
+            $table->enum('featured',['is_featured', 'is_latest', 'is_trending', 'is_best_rated', 'is_most_viewed']);
+            $table->enum('status',[1, 0])->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/seeders/ProductTableSeeder.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $product = [
+          [
+              'category_id' => 1,
+              'section_id' => 1,
+              'brand_id' => 1,
+              'vendor_id' => null,
+              'admin_type' => 'superadmin',
+              'product_name' => 'Apple iPhone 12 Pro Max',
+              'product_color' => 'Silver',
+              'product_code' => 'A12PM',
+              'product_price' => 1399,
+              'product_discount' => 0,
+              'product_weight' => 0.5,
+              'product_image' => 'https://m.media-amazon.com/images/I/71MHTD3uL4L._AC_UY218_.jpg',
+              'description' => '6.7-inch Super Retina XDR display Ceramic Shield with 4x better drop performance than any smartphone glass Industry-leading IP68 water resistance Tougher front and back glass, tougher stainless steel band, tougher ceramic shield',
+              'meta_title' => 'Apple iPhone 12 Pro Max',
+              'meta_keywords' => 'Apple iPhone 12 Pro Max',
+              'meta_description' => 'Apple iPhone 12 Pro Max',
+              'featured' => 'is_featured',
+              'status' => 1,
+              'created_at' => now(),
+              'updated_at' => now(),
+          ],
+            [
+                'category_id' => 1,
+                'section_id' => 1,
+                'brand_id' => 1,
+                'vendor_id' => 1,
+                'admin_type' => 'vendor',
+                'product_name' => 'Apple iPhone 12 Pro',
+                'product_color' => 'Silver',
+                'product_code' => 'A12P',
+                'product_price' => 1199,
+                'product_discount' => 0,
+                'product_weight' => 0.5,
+                'product_image' => 'https://m.media-amazon.com/images/I/71MHTD3uL4L._AC_UY218_.jpg',
+                'description' => '6.1-inch Super Retina XDR display Ceramic Shield with 4x better drop performance than any smartphone glass Industry-leading IP68 water resistance Tougher front and back glass, tougher stainless steel band, tougher ceramic shield',
+                'meta_title' => 'Apple iPhone 12 Pro',
+                'meta_keywords' => 'Apple iPhone 12 Pro',
+                'meta_description' => 'Apple iPhone 12 Pro',
+                'featured' => 'is_featured',
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'category_id' => 2,
+                'section_id' => 2,
+                'brand_id' => 2,
+                'vendor_id' => 1,
+                'admin_type' => 'vendor',
+                'product_name' => 'Apple iPhone 12',
+                'product_color' => 'Silver',
+                'product_code' => 'A12',
+                'product_price' => 999,
+                'product_discount' => 0,
+                'product_weight' => 0.5,
+                'product_image' => 'https://m.media-amazon.com/images/I/71MHTD3uL4L._AC_UY218_.jpg',
+                'description' => '6.1-inch Super Retina XDR display Ceramic Shield with 4x better drop performance than any smartphone glass Industry-leading IP68 water resistance Tougher front and back glass, tougher stainless steel band, tougher ceramic shield',
+                'meta_title' => 'Apple iPhone 12',
+                'meta_keywords' => 'Apple iPhone 12',
+                'meta_description' => 'Apple iPhone 12',
+                'featured' => 'is_featured',
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+
+            ],
+            [
+                'category_id' => 2,
+                'section_id' => 2,
+                'brand_id' => 2,
+                'vendor_id' => 1,
+                'admin_type' => 'vendor',
+                'product_name' => 'Apple iPhone 12 Mini',
+                'product_color' => 'Silver',
+                'product_code' => 'A12M',
+                'product_price' => 799,
+                'product_discount' => 0,
+                'product_weight' => 0.5,
+                'product_image' => 'https://m.media-amazon.com/images/I/71MHTD3uL4L._AC_UY218_.jpg',
+                'description' => '5.4-inch Super Retina XDR display Ceramic Shield with 4x better drop performance than any smartphone glass Industry-leading IP68 water resistance Tougher front and back glass, tougher stainless steel band, tougher ceramic shield',
+                'meta_title' => 'Apple iPhone 12 Mini',
+                'meta_keywords' => 'Apple iPhone 12 Mini',
+                'meta_description' => 'Apple iPhone 12 Mini',
+                'featured' => 'is_featured',
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+
+            ]
+        ];
+
+        Product::insert($product);
+    }
+}
